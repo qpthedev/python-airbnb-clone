689d1d8b8b3bccef62786e3a6e45936fff9018fc
qpthedev <qpthedev@gmail.com>
2020-09-04 06:24:44 -0400

    edit rooms app fieldsets and related name

--- rooms/admin.py
+++ rooms/admin.py
@@ -7,14 +7,87 @@ from . import models
 class RoomAdmin(admin.ModelAdmin):
     """ Room Admin Definition """
 
-    pass
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "address", "price",)},
+        ),
+        ("Times", {"fields": ("check_in", "check_out", "instant_book",)}),
+        ("Spaces", {"fields": ("guests", "beds", "bedrooms", "baths",)}),
+        (
+            "More About the space",
+            {
+                "classes": ("collapse",),
+                "fields": ("amenities", "facilities", "house_rule",),
+            },
+        ),
+        ("Last Details", {"fields": ("host",)}),
+    )
+
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+        "count_amenities",
+        "count_photos",
+    )
+
+    ordering = [
+        "price",
+        "bedrooms",
+    ]
+
+    list_filter = (
+        "instant_book",
+        "host__superhost",
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rule",
+        "city",
+        "country",
+    )
+
+    search_fields = [
+        "country",
+        "city",
+        "^host__username",
+    ]
+
+    filter_horizontal = [
+        "amenities",
+        "facilities",
+        "house_rule",
+    ]
+
+    def count_amenities(self, obj):
+        return obj.amenities.count()
+
+    # count_amenities.short_description = "Amenities"
+
+    def count_photos(self, obj):
+        return obj.photos.count()
 
 
 @admin.register(models.RoomType, models.Amenity, models.Facility, models.HouseRule)
 class ItemAdmin(admin.ModelAdmin):
     """ Item Admin Definition """
 
-    pass
+    list_display = (
+        "name",
+        "used_by",
+    )
+
+    def used_by(self, obj):
+        return obj.rooms.count()
 
 
 @admin.register(models.Photo)
