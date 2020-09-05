05f87097533ace8f4f1d5d688660e40e882e253e
qpthedev <qpthedev@gmail.com>
2020-09-05 06:34:22 -0400

    show review rating avg on rooms admin

--- rooms/models.py
+++ rooms/models.py
@@ -84,3 +84,14 @@ class Room(core_models.TimeStampedModel):
 
     def __str__(self):
         return self.name
+
+    def total_rating(self):
+        all_reviews = self.reviews.all()
+        all_ratings = 0
+        if all_reviews:
+            for review in all_reviews:
+                all_ratings += review.rating_average()
+
+            return all_ratings / len(all_reviews)
+        elif not all_reviews:
+            return 0
