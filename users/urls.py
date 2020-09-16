2d7b77359bb904cb1121f99db61b3d2b381ab89a
qpthedev <qpthedev@gmail.com>
2020-09-16 04:25:34 -0400

    user authentication, log in, log out

--- users/urls.py
+++ users/urls.py
@@ -6,4 +6,5 @@ app_name = "users"
 urlpatterns = [
     path("", views.all_users, name="users"),
     path("login", views.LoginView.as_view(), name="login"),
+    path("logout", views.log_out, name="logout"),
 ]
