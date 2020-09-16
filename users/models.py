2cb821fd6b18b75d212a5bf1eab27a1a8e885890
qpthedev <qpthedev@gmail.com>
2020-09-16 06:01:51 -0400

    change the sign up form to use ModelForm

--- users/models.py
+++ users/models.py
@@ -37,6 +37,10 @@ class User(AbstractUser):
     gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
     bio = models.TextField(blank=True)
     birthdate = models.DateField(blank=True, null=True)
-    language = models.CharField(choices=LANGUAGE_CHOICES, blank=True, max_length=2)
-    currency = models.CharField(choices=CURRENCY_CHOICES, blank=True, max_length=3)
+    language = models.CharField(
+        choices=LANGUAGE_CHOICES, blank=True, max_length=2, default=LANGUAGE_ENGLISH
+    )
+    currency = models.CharField(
+        choices=CURRENCY_CHOICES, blank=True, max_length=3, default=CURRENCY_USD
+    )
     superhost = models.BooleanField(default=False)
