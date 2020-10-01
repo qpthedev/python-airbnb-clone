c42bf7d1546da56908c01b63f48da647cd0f7aaa
qpthedev <qpthedev@gmail.com>
2020-10-01 05:53:22 -0400

    filter with function based view

--- rooms/views.py
+++ rooms/views.py
@@ -24,15 +24,15 @@ class RoomDetail(DetailView):
 def search(request):
     city = request.GET.get("city", "Anywhere")
     city = str.capitalize(city)
-    country = request.GET.get("country", "US")
+    country = request.GET.get("country", "")
     room_type = int(request.GET.get("room_type", 0))
     price = int(request.GET.get("price", 0))
     guests = int(request.GET.get("guests", 0))
     bedrooms = int(request.GET.get("bedrooms", 0))
     beds = int(request.GET.get("beds", 0))
     baths = int(request.GET.get("baths", 0))
-    instant = request.GET.get("super_host", False)
-    super_host = request.GET.get("instant", False)
+    instant = bool(request.GET.get("super_host", False))
+    superhost = bool(request.GET.get("instant", False))
     s_amenities = request.GET.getlist("amenities")
     s_facilities = request.GET.getlist("facilities")
 
@@ -45,7 +45,7 @@ def search(request):
         "bedrooms": bedrooms,
         "beds": beds,
         "baths": baths,
-        "super_host": super_host,
+        "superhost": superhost,
         "instant": instant,
     }
 
@@ -62,8 +62,55 @@ def search(request):
         "s_facilities": s_facilities,
     }
 
+    filter_args = {}
+
+    if city != "Anywhere":
+        filter_args["city__startswith"] = city
+
+    filter_args["country"] = country
+
+    # * Connecting via Foreign Key
+    if room_type != 0:
+        filter_args["room_type__pk"] = room_type
+
+    if price != 0:
+        filter_args["price__lte"] = price
+
+    if guests != 0:
+        filter_args["guests__gte"] = guests
+
+    if bedrooms != 0:
+        filter_args["bedrooms__gte"] = bedrooms
+
+    if beds != 0:
+        filter_args["beds__gte"] = beds
+
+    if baths != 0:
+        filter_args["baths__gte"] = baths
+
+    if instant is True:
+        filter_args["instant_book"] = True
+
+    # * Connecting via Foreign Key
+    if superhost is True:
+        filter_args["host__superhost"] = True
+
+    if len(s_amenities) > 0:
+        for s_amenity in s_amenities:
+            filter_args["amenities__pk"] = int(s_amenity)
+
+    if len(s_facilities) > 0:
+        for s_facility in s_facilities:
+            filter_args["facilities__pk"] = int(s_facility)
+
+    rooms = models.Room.objects.filter(**filter_args)
+
     return render(
         request,
         "rooms/search.html",
-        {**form, **choices},
+        {
+            **form,
+            **choices,
+            "rooms": rooms,
+        },
     )
