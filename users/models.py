1b0cf7d42817a811afe3c477a2ea930b6a9bbdc5
qpthedev <qpthedev@gmail.com>
2020-10-03 16:13:32 -0400

    user email verification

--- users/models.py
+++ users/models.py
@@ -1,6 +1,11 @@
+import uuid
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.shortcuts import reverse
+from django.core.mail import send_mail
+from django.utils.html import strip_tags
 from django.db import models
+from django.template.loader import render_to_string
 
 
 # Create your models here.
@@ -45,6 +50,27 @@ class User(AbstractUser):
         choices=CURRENCY_CHOICES, blank=True, max_length=3, default=CURRENCY_USD
     )
     superhost = models.BooleanField(default=False)
+
+    email_verified = models.BooleanField(default=False)
+    email_secret = models.CharField(max_length=20, default="", blank=True)
+
+    def verify_email(self):
+        if self.email_verified is False:
+            secret = uuid.uuid4().hex[:20]
+            self.email_secret = secret
+            html_message = render_to_string(
+                "emails/verify_email.html",
+                {"secret": secret},
+            )
+            send_mail(
+                "Verify Airbnb Account",
+                strip_tags(html_message),
+                settings.EMAIL_FROM,
+                [self.email],
+                fail_silently=False,
+                html_message=html_message,
+            )
+        return
 
     def get_absolute_url(self):
         return reverse("users:profile", kwargs={"pk": self.pk})
