03b88e8e8f0eceaa426d2908a5b52a5e217f93f0
qpthedev <qpthedev@gmail.com>
2020-10-09 16:23:44 -0400

    UserCreationForm for pw validaton and simpler code

--- users/views.py
+++ users/views.py
@@ -7,6 +7,7 @@ from django.shortcuts import redirect, reverse
 from django.urls import reverse_lazy
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.messages.views import SuccessMessageMixin
+from django.core.files.base import ContentFile
 from . import forms, models, mixins
 
 
@@ -234,6 +235,11 @@ def kakao_callback(request):
             )
             user.set_unusable_password()
             user.save()
+            if profile_image is not None:
+                photo_request = requests.get(profile_image)
+                user.avatar.save(
+                    f"{nickname}-avatar", ContentFile(photo_request.content)
+                )
         login(request, user)
         return redirect(reverse("core:home"))
     except KakaoException:
