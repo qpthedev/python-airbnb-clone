0fa35177eb7bae636b2584c7a2aa7e21e621d3a6
qpthedev <qpthedev@gmail.com>
2020-10-15 23:10:52 -0400

    front page styling with tailwindcss

--- rooms/models.py
+++ rooms/models.py
@@ -100,6 +100,10 @@ class Room(core_models.TimeStampedModel):
             for review in all_reviews:
                 all_ratings += review.rating_average()
 
-            return round(all_ratings / len(all_reviews))
+            return round(all_ratings / len(all_reviews), 2)
         elif not all_reviews:
             return 0
+
+    def first_photo(self):
+        (photo,) = self.photos.all()[:1]
+        return photo.file.url
